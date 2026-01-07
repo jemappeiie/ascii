453d0eab581975c9372231366b40b2dcd46bc001
Luka Chernyshov <luka.chernyshov@outlook.com>
2026-01-07 02:58:38 +0100

    Add file saving; rename --path to --input and include parameter help messages

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,11 +10,13 @@ import com.github.ajalt.clikt.parameters.types.int
 import kotlinx.coroutines.runBlocking
 
 class Main : CliktCommand() {
-    val path by option("--path").required()
-    val width by option("--width").int().default(100)
-    val height by option("--height").int()
-    val invert by option("--invert").flag()
-    val image by lazy { ImageUtils.loadImage(path).getOrThrow() }
+    val input by option("--input", help = "Path to the source file").required()
+    val output by option("--output", help = "Path to the destination file; prints to console if omitted")
+    val width by option("--width", help = "Target width for the output").int().default(100)
+    val height by option("--height", help = "Target height for the output").int()
+    val invert by option("--invert", help = "Invert the luminance of the output")
+        .flag()
+    val image by lazy { ImageUtils.loadImage(input).getOrThrow() }
     val config = Config.default()
     override fun run() {
         val resizedImage = height?.let {
@@ -23,7 +25,9 @@ class Main : CliktCommand() {
         val processor = Processor(config)
         runBlocking {
             val ascii = processor.process(resizedImage, invert)
-            println(ascii)
+            output?.let {
+                AsciiUtils.saveAscii(it, ascii)
+            } ?: println(ascii)
         }
     }
 }
